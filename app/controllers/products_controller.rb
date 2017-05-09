024289d7442edb394c16be9b687f6af2d96582fd
Maxime3298 <maxime3298@gmail.com>
2017-05-09 22:01:04 +0200

    add model Product

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,26 @@
+class ProductsController < ApplicationController
+
+#  PRODUCTS = [
+#               {name: "Kudoz", category: "Pro"},
+#                {name: "uSlide", category: "Education"},
+#                {name: "Hubum", category: "Lifestyle"},
+#                {name: "Roadstr", category: "Lifestyle"}
+#             ]
+def index
+  # @products = ["kudoz", "uSlide", "Hubum", "Roadstr"]
+  # render plain: products.join(", ")
+#   if params[:filter]
+#    @products = PRODUCTS.select {|product| product[:category] == params[:filter]}
+#   else
+#    @products = PRODUCTS
+#  end
+  @products = Product.all
+end
+
+def show
+#  @product = PRODUCTS[params[:id].to_i]
+  @product = Product.find(params[:id])
+end
+
+
+end
